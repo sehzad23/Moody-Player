b0a6d1d79aa3aa3ed51960b3fdd8a83c9626a6f9
sehzad23 <sehaans87@gmail.com>
2025-08-16 17:04:54 +0530

    Fix formatting and clean up app.js by adding missing semicolons and adjusting whitespace

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,10 @@
 // src/app.js
 
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const songrouter = require("../src/routes/song.route");
 
-
 const app = express();
-
 
 // ✅ Enable CORS for all routes
 app.use(cors());
@@ -14,6 +12,10 @@ app.use(cors());
 // Middleware
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Music App API");
+});
 
 //routes
 app.use("/", songrouter);
